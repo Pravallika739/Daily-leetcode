eca2ab909f98780f14748cead768a2703e296401
BANDARU PRAVALLIKA <167642731+Pravallika739@users.noreply.github.com>
2026-08-13 12:29:25 +0530

    Time: 1 ms (82.85%), Space: 19.3 MB (65.9%) - LeetHub

--- 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.py
+++ 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.py
@@ -1,15 +1,22 @@
 class Solution:
     def countKConstraintSubstrings(self, s: str, k: int) -> int:
         res=0
-        for i in range(len(s)):
-            zeroes=0
-            ones=0
-            for j in range(i,len(s)):
-                if s[j]=='0':
-                   zeroes+=1
-                else:
-                   ones+=1
-                if zeroes>k and ones>k:
-                    break
-                res+=1
+        zeroes=0
+        ones=0
+        i=0
+        l=0
+        while i<len(s):
+            if s[i]=='0':
+                zeroes+=1
+            else:
+                ones+=1
+            if zeroes>k and ones>k:
+                while zeroes>k and ones>k:
+                    if s[l]=='0':
+                        zeroes-=1
+                    else:
+                        ones-=1
+                    l+=1
+            res+=(i-l+1)
+            i+=1
         return res
