35c258ade88a0b1e6b1e293e5afa47754f1c5472
BANDARU PRAVALLIKA <167642731+Pravallika739@users.noreply.github.com>
2026-08-13 13:18:17 +0530

    Time: 375 ms (5.13%), Space: 64.4 MB (5.13%) - LeetHub

---
+++ 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.py
@@ -0,0 +1,26 @@
+class Solution:
+    def getStrongest(self, a: List[int], k: int) -> List[int]:
+        l=[]
+        a.sort()
+        m=a[(len(a)-1)//2]
+        print(m)
+        for i in a:
+            l.append(abs(i-m))
+        d={}
+        freq={}
+        for i in a:
+            if i not in freq:
+                freq[i]=1
+            else:
+                freq[i]+=1
+        c=0
+        for i in range(len(a)):
+                d[a[i]]=l[i]
+        d=dict(sorted(d.items(),key=lambda x:(-x[1],-x[0])))
+        print(d)
+        res=[]
+        for i in d:
+            while freq[i]>0:
+                res.append(i)
+                freq[i]-=1
+        return res[:k]
